4c186a30aec7ddaef382342556c67bd406bee250
maciej-kuchejda <kuchejdamaciej@gmail.com>
2022-12-30 12:08:28 +0100

    exercise 5

--- cleaner.ps1
+++ cleaner.ps1
@@ -1,4 +1,4 @@
 Get-ChildItem .\ -include bin,obj -Recurse | foreach ($_) { remove-item $_.fullname -Force -Recurse }
 
-$zipName = 'Maciej Kuchejda-exercise4-wariant4'
+$zipName = 'Maciej Kuchejda-exercise5-wariant4b'
 Compress-Archive -Path * $zipName
